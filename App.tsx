19081fba1da108120acff453ef6505f0b0302247
Mateo Hrastnik <mateo@lloyds.design>
2022-02-13 22:20:58 +0100

    Fix telefuncConfig import

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Text, View, DevSettings } from "react-native";
+import { Text, View } from "react-native";
+import { telefuncConfig } from "telefunc/dist/client";
 import { readPost } from "./readPost.telefunc";
-import { telefuncConfig } from "telefunc/client";
 
 telefuncConfig.telefuncUrl = "http://localhost:3000/_telefunc"
 
