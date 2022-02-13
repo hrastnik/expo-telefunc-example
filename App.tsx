5104fc0497181fd1fcd4f9f2fd1065992e7e2dc6
Mateo Hrastnik <mateo@lloyds.design>
2022-02-13 18:14:13 +0100

    Fix imports for babel-plugin and client

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Text, View, DevSettings } from "react-native";
 import { readPost } from "./readPost.telefunc";
-import { telefuncConfig } from "telefunc/dist/client";
+import { telefuncConfig } from "telefunc/client";
 
 telefuncConfig.telefuncUrl = "http://localhost:3000/_telefunc"
 
